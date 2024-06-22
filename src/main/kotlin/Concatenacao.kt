1d13fcb2b7551e53a77a5800e586a820a6bc54ef
Lucca Brito Gesteira <luccabritogt@gmail.com>
2024-06-22 15:58:45 -0300

    Aula 3 - Concatenação

---
+++ src/main/kotlin/Concatenacao.kt
@@ -0,0 +1,10 @@
+class Concatenacao {
+
+    fun main() {
+        var nome = "Lucca"
+        var idade = 28
+        var profissao = "Desenvolvedor de Software"
+
+        println("Meu nome é $nome, eu tenho $idade anos e trabalho como $profissao")
+    }
+}
