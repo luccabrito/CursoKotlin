46de398e19562922e098e73a5ffe7603713a2bfb
Lucca Brito Gesteira <luccabritogt@gmail.com>
2024-06-22 16:17:40 -0300

    Aula 5 - Operadores Relacionais

---
+++ src/main/kotlin/OperadoresRelacionais.kt
@@ -0,0 +1,16 @@
+class OperadoresRelacionais {
+    fun main() {
+        val a = 10
+        val b = 5
+        val c = 10
+
+        // Operadores relacionais
+        println("$a == $b: ${a == b}") // Igual
+        println("$a == $c: ${a == c}") // Igual
+        println("$a != $b: ${a != b}") // Diferente
+        println("$a < $b: ${a < b}")   // Menor que
+        println("$a > $b: ${a > b}")   // Maior que
+        println("$a <= $b: ${a <= b}") // Menor ou igual a
+        println("$a >= $b: ${a >= b}") // Maior ou igual a
+    }
+}
