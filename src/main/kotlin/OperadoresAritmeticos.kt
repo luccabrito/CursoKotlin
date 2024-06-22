75b7be0dc6dcb282709cf8138fdd95fdbe555ce6
Lucca Brito Gesteira <luccabritogt@gmail.com>
2024-06-22 16:11:32 -0300

    Aula 4 - Operadores Aritméticos

---
+++ src/main/kotlin/OperadoresAritmeticos.kt
@@ -0,0 +1,22 @@
+class OperadoresAritmeticos {
+    fun main(args: Array<String>) {
+        // Declaração de variáveis
+        var a = 10
+        var b = 5
+
+        // Operações aritméticas
+        val soma = a + b
+        val subtracao = a - b
+        val multiplicacao = a * b
+        val divisao = a / b
+        val modulo = a % b
+
+        // Incremento e decremento
+        var c = 15
+        println(c++) // 15
+        println(++c) // 17
+        var d = 20
+        println(--d)  // 19
+        println(d--) // 19
+    }
+}
