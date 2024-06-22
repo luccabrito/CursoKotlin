6b176390ba462d9518984161f3891b6ff74cddf0
Lucca Brito Gesteira <luccabritogt@gmail.com>
2024-06-22 15:48:36 -0300

    Aula 2 - Tipos de Variáveis

---
+++ src/main/kotlin/TiposVariaveis.kt
@@ -0,0 +1,19 @@
+class TiposVariaveis {
+    fun main(args: Array<String>) {
+        // Tipos de Inteiros
+        val byteVar: Byte = 10
+        val shortVar: Short = 20
+        val intVar: Int = 30
+        val longVar: Long = 40L // O sufixo L indica que é do tipo Long
+
+        // Tipos de Ponto Flutuante
+        val floatVar: Float = 3.14f // O sufixo f indica que é do tipo Float - até 7 casas decimais
+        val doubleVar: Double = 1.414
+
+        // Tipo de Caractere
+        val charVar: Char = 'A'
+
+        // Tipo Booleano
+        val booleanVar: Boolean = true
+    }
+}
