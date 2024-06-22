5288f5cd7bbc43f0bc2ef583b7f6a475664f2e52
Lucca Brito Gesteira <luccabritogt@gmail.com>
2024-06-22 14:41:00 -0300

    Aula 1 - Variável mutável e imutável

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    // Variável mutável
+    var nome = "Lucca Brito Gesteira"
+    nome = "Clara"
+
+    // Variável imutável
+    val idade = 28
+
+    println(idade)
+}
